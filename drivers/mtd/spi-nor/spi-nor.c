b18adeb1e60bba3d6aa77e302673b38c41fd5c77
florpor <florist.powers@gmail.com>
2020-04-30 14:28:21 +0300

    add xtx chip id to spi-nor-ids
    some licheepi nanos come with an xtx chips instead of a winbond ones. it is compatible enough and needs no other code changes

--- drivers/mtd/spi-nor/spi-nor.c
+++ drivers/mtd/spi-nor/spi-nor.c
@@ -2486,6 +2486,9 @@ static const struct flash_info spi_nor_ids[] = {
 	{ "w25m512jv", INFO(0xef7119, 0, 64 * 1024, 1024,
 			SECT_4K | SPI_NOR_QUAD_READ | SPI_NOR_DUAL_READ) },
 
+	/* XTX */
+	{ "xt25f128b", INFO(0x0b4018, 0, 64 * 1024, 256, SECT_4K) },
+
 	/* Catalyst / On Semiconductor -- non-JEDEC */
 	{ "cat25c11", CAT25_INFO(  16, 8, 16, 1, SPI_NOR_NO_ERASE | SPI_NOR_NO_FR) },
 	{ "cat25c03", CAT25_INFO(  32, 8, 16, 2, SPI_NOR_NO_ERASE | SPI_NOR_NO_FR) },
